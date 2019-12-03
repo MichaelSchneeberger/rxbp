d897fac3bfd50d3bbfada0c980680c22d9e36ed1
mike <michael.schneeb@gmail.com>
2019-12-03 22:10:18 +0100

    add requirement.txt

--- rxbp/observables/filterobservable.py
+++ rxbp/observables/filterobservable.py
@@ -1,17 +1,9 @@
-import functools
 from typing import Callable, Any
 
-from rxbp.ack.ack import Ack
-from rxbp.ack.ackimpl import stop_ack
-from rxbp.ack.merge import _merge
 from rxbp.observerinfo import ObserverInfo
 from rxbp.observers.filterobserver import FilterObserver
-from rxbp.selectors.selectionmsg import select_next, select_completed
 from rxbp.observable import Observable
-from rxbp.observer import Observer
 from rxbp.scheduler import Scheduler
-from rxbp.observablesubjects.publishosubject import PublishOSubject
-from rxbp.typing import ElementType
 
 
 class FilterObservable(Observable):
--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import setup, find_packages
-import unittest
-
-
-def my_test_suite():
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    return test_suite
-
 
 setup(
     name='rxbp',
@@ -17,5 +9,4 @@ setup(
     author='Michael Schneeberger',
     author_email='michael.schneeb@outlook.com',
     download_url='https://github.com/MichaelSchneeberger/rxbackpressure',
-    test_suite='setup.my_test_suite',
 )
