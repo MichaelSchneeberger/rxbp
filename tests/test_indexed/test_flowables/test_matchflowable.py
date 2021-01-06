c52b0aaee9d11bf5e8b958eccfbd3b3216afa1c2
Schneeberger Michael <michael.schneeberger@fhnw.ch>
2021-01-06 10:25:40 +0100

    fix imports

--- tests/test_indexed/test_flowables/test_matchflowable.py
+++ tests/test_indexed/test_flowables/test_matchflowable.py
@@ -1,4 +1,4 @@
-from rxbp.indexed.flowables.matchflowable import IndexedMatchFlowable
+from rxbp.indexed.flowables.matchindexedflowable import MatchIndexedFlowable
 from rxbp.indexed.selectors.bases.numericalbase import NumericalBase
 from rxbp.subscriber import Subscriber
 from rxbp.testing.testcasebase import TestCaseBase
@@ -23,7 +23,7 @@ class TestMatchFlowable(TestCaseBase):
         s1 = TestFlowable(base=b1, selectors={b3: None})
         s2 = TestFlowable(base=b2, selectors={b4: None})
 
-        flowable = IndexedMatchFlowable(
+        flowable = MatchIndexedFlowable(
             left=s1,
             right=s2,
         )
@@ -44,7 +44,7 @@ class TestMatchFlowable(TestCaseBase):
         s1 = TestFlowable(base=b1, selectors={b3: 'sel3'})
         s2 = TestFlowable(base=b2, selectors={b1: 'sel1', b4: 'sel4'})
 
-        flowable = IndexedMatchFlowable(
+        flowable = MatchIndexedFlowable(
             left=s1,
             right=s2,
         )
